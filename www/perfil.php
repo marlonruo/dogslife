eee328ef91c487e42aa2b659eff8b9aba99d3401
Marlon Ruiz <marlon_ru@hotmail.com>
2017-02-27 18:31:01 -0600

    ir

--- www/perfil.php
+++ www/perfil.php
@@ -1,8 +1,17 @@
+<?php
+header("Expires: Tue, 03 Jul 2001 06:00:00 GMT");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+header('Content-Type: text/html; charset=UTF-8');
+?>
+
 				<div id="p_foto" style="position:absolute; top:8%; width:100%; height:40%; background:url(img/sin_foto.png) no-repeat; background-position:center; background-size:cover">
                 	<div id="ir_mascota" style="position:absolute; width:100%; height:30%; background-color:rgba(0,0,0,.5); bottom:0px; cursor:pointer">
                     	<img src="img/mascota.png" class="centrar_v" style="right:5%; height:55%;"/>
                         <div class="centrar_v" style="right:20%; text-align:right; font-size:13px; color:#FFFFFF">
-                        	<div style="font-size:15px" class="texto18"><b>Perfil de</b></div>
+                        	<div style="font-size:15px" class="texto18"><b>Perfil del aa</b></div>
                             <div class="texto15">mi mascota</div>
                         </div>
                         <div class="centrar_v" style="left:8%; text-align:left; font-size:13px; color:#FFFFFF"><b style="font-size:18px" class="texto18">12</b><br><span class="texto14">amigos</span></div>
